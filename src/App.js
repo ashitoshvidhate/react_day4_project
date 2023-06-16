b3aadd62081286aec2a38224bda736321742acc2
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-16 21:53:18 +0530

    stateful_list_adding_data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import { useState } from "react";
+
 function App() {
   return (
     <>
-      <h1>List Demo</h1>
+      <h1>Stateful List</h1>
       <ListDemo />
     </>
   );
 }
 
-function ListDemo() {}
+function ListDemo() {
+  //let list=["delhi"];
+  //c1:stateful variable
+  let [list, setList] = useState(["Delhi"]);
+
+  //c2:Action Member Function
+  let addItemAction = () => {
+    //creating the clone of list and adding the mumbai in it
+    let newlist = [...list, "Mumbai"];
+
+    console.log(newlist);
+
+    //DOM :: TRIGGER DOM
+    setList(newlist);
+  };
+
+  return (
+    <>
+      {/**C3 : Event Binding */}
+      <input type="button" value="Add New Item" onClick={addItemAction} />
+      {list.map((item) => (
+        <h1>Hello {item}</h1>
+      ))}
+    </>
+  );
+}
 
 export default App;
