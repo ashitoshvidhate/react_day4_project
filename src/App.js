b43fc11873a02a9a7cc12fa0108009293e1312f5
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-16 21:02:53 +0530

    list_react

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ function App() {
   return (
     <>
       <h1>Props Demo</h1>
-
-      {/**Calling the MessageDemo{1,'abcd'} */}
-
-      <MessageDemo name="Vitthal" />
-      <MessageDemo name="Ashitosh" />
-      <MessageDemo name="CDAC" />
+      <ListDemo />
     </>
   );
 }
 
-function MessageDemo(props) {
-  //console.log("I am inside messagedemo", props);
+function ListDemo() {
+  let data = "Hello Universe";
+  let list = ["delhi", "Mumbai", "Chennai"];
+
   return (
     <>
-      <h1>Hello {props.name}</h1>
+      <h1>Data</h1>
+
+      {/**Version 3 */}
+      {list.map((item) => (
+        <>
+          <h1>Hello World</h1>
+        </>
+      ))}
+
+      {/**version 2.2*/}
+      {list.map((item) => (
+        <h1>Hello {item}</h1>
+      ))}
+
+      {/** version 2.1 */}
+      {list.map((item) => "Hello" + item)}
+
+      {/**version 1 */}
+      {list.map((item) => item)}
     </>
   );
 }
