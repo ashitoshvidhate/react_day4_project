9a0fcf378ec94dc814d7ce3a4c004111da3018e4
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-17 00:25:51 +0530

    thumsup_down_demo

--- src/App.js
+++ src/App.js
@@ -35,22 +35,21 @@ function ListDemo() {
       <input type="button" value="Add New Item" onClick={addItemAction} />
 
       {list.map((item) => (
-        <div>
-          <h1>{item}</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt rem
-            quasi modi. Eveniet, voluptates quibusdam vel illum iure aliquid
-            similique minus recusandae quas maiores reiciendis voluptatem!
-            Libero assumenda cumque atque!
-          </p>
-          <div>
-            <input type="button" value="&#128077;" />
-            <input type="button" value="&#128078;" />
-          </div>
-        </div>
+        <MessageDemo />
       ))}
     </>
   );
 }
 
+function MessageDemo() {
+  return (
+    <>
+      <h1>Hello from WPT</h1>
+      <div>
+        <input type="button" value="&#128077;" />
+        <input type="button" value="&#128078;" />
+      </div>
+    </>
+  );
+}
 export default App;
