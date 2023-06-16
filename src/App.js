ce9fecaef50d00b4a4c83a67b43c1afc9ffd08c2
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-16 21:23:57 +0530

    list1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 function App() {
   return (
     <>
-      <h1>Props Demo</h1>
+      <h1>List Demo</h1>
       <ListDemo />
     </>
   );
 }
 
-function ListDemo() {
-  let data = "Hello Universe";
-  let list = ["delhi", "Mumbai", "Chennai"];
-
-  return (
-    <>
-      <h1>Data</h1>
-
-      {/**Version 3 */}
-      {list.map((item) => (
-        <>
-          <h1>Hello World</h1>
-        </>
-      ))}
-
-      {/**version 2.2*/}
-      {list.map((item) => (
-        <h1>Hello {item}</h1>
-      ))}
-
-      {/** version 2.1 */}
-      {list.map((item) => "Hello" + item)}
-
-      {/**version 1 */}
-      {list.map((item) => item)}
-    </>
-  );
-}
+function ListDemo() {}
 
 export default App;
